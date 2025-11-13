6d4daa158df450b988a5654e41111b7b190e0893
RAMIRO <ramiromoises2019@gmail.com>
2025-11-13 20:00:04 -0300

    adaptacion a mobile y menu haamburguesa

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+// Selección de elementos
+const btn = document.getElementById("hamburguesa");
+const menu = document.getElementById("menu");
+
+// Evento para abrir/cerrar menú
+btn.addEventListener("click", () => {
+    menu.classList.toggle("menu-activo");
+    btn.classList.toggle("activo");
+});
